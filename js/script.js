58fc74d581d143a21b29ce3a53d1c03201ef1028
SalmanMukhtarFED <54955262+SalmanMukhtarFED@users.noreply.github.com>
2021-02-27 23:44:31 +0500

    Super Nova Bootstrap Carousel

---
+++ js/script.js
@@ -0,0 +1,27 @@
+
+$(document).ready(function () {
+//Allows bootstrap carousels to display 3 items per page rather than just one
+$(function () {
+  $("#carousel-multiple").on("slide.bs.carousel", function (e) {
+	 
+    var itemsPerSlide = parseInt($(this).attr('data-maximum-items-per-slide')),
+      totalItems = $(".carousel-item", this).length,
+      reserve = 1,//do not change
+      $itemsContainer = $(".carousel-inner", this),
+      it = (itemsPerSlide + reserve) - (totalItems - e.to);
+
+    if (it > 0) {
+      for (var i = 0; i < it; i++) {
+        $(".carousel-item", this)
+          .eq(e.direction == "left" ? i : 0)
+          // append slides to the end/beginning
+          .appendTo($itemsContainer);
+      }
+    }
+	interval: 2000
+  });
+});
+
+  
+});
+
